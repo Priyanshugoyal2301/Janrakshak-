2a35a2fe9e9720b3e3cdd54943930e55132cbd59
Kabilesh C <kc7002@srmist.edu.in>
2025-12-07 18:09:11 +0530

    Add admin flood prediction accuracy dashboard

---
+++ src/pages/AdminFloodAccuracy.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "@/components/AdminLayout";
+import TamilNaduFloodAccuracy from "@/components/TamilNaduFloodAccuracy";
+
+const AdminFloodAccuracy = () => {
+  return (
+    <AdminLayout>
+      <TamilNaduFloodAccuracy />
+    </AdminLayout>
+  );
+};
+
+export default AdminFloodAccuracy;
